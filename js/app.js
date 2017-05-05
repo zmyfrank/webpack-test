43c5c20eaff7922c1a693b59ecf58c554ec3182e
issuser <issuser>
2017-05-05 17:01:27 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by issuser on 2017/5/5.
+ */
+angular.module('app',[
+	'ui-router',
+	'oc.lazyLoad'])
+	
+.config()
+//可以配置http设置
+function httpConfig($httpProvider) {
+	//统一设置content-type
+	$httpProvider.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	$httpProvider.defaults.headers.patch['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	$httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+	
+	//统一设置transformRequest预处理方法
+	$httpProvider.defaults.transformRequest = function (data) {
+		if (data === undefined) {
+			return data;
+		}
+		return $.param(data);
+	};
+}
+
